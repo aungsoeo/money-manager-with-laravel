85487617d3261d943c2fbf9971fd2d76578a6f09
Aung Soe Oo <aungsoeo@users.noreply.github.com>
2018-07-05 14:11:04 +0630

    add missing column

--- database/migrations/2017_08_13_103625_create_income_table.php
+++ database/migrations/2017_08_13_103625_create_income_table.php
@@ -18,7 +18,9 @@ class CreateIncomeTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->string('income_name');
             $table->integer('in_amount');
+            $table->string('outcome_name');
             $table->integer('out_amount');
+            $table->string('save_name');
             $table->integer('sav_amount');
             $table->date('income_date');
             $table->timestamps();
