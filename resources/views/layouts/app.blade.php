5830ca0c9fea20266d2e17f764a79e3eba29528a
Aung Soe Oo <aungsoeo@users.noreply.github.com>
2018-09-20 00:38:48 +0630

    remove

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,17 +112,7 @@
                                     </li>
                                 </ul>
                             </li>
-                                                                <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
+                                                               
 
                         @endif
                     </ul>
