923a372ae06415fb0662f66f1978653146985341
Aung Soe Oo <aungsoeo@users.noreply.github.com>
2021-08-14 17:04:56 +0630

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer id="fh5co-footer">
-  <p><small>&copy; 2017. All Rights Reserverd. <br> Designed by <a href="#" target="_blank">ေအာင္စိုးဦး</a></small></p>
+  <p><small>&copy; 2017. All Rights Reserverd. <br> Designed by <a href="#" target="_blank">အောင်စိုးဦး</a></small></p>
 </footer>
